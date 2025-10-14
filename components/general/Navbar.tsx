a0733ca884168d6e6360ce1e8975739b3bb60e2b
alexPetkov92 <a.petkov@webrika.bg>
2025-10-14 15:23:35 +0300

    Created ThemeProvider and Navbar

---
+++ components/general/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { BriefcaseBusiness } from "lucide-react";
+import { Button } from "../ui/button";
+import { ModeToggle } from "./ModeToggle";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between py-5">
+      <Link href="/" className="flex items-center gap-2">
+        <BriefcaseBusiness className="px-2 py-2 w-10 h-10 bg-radial-[at_50%_75%] from-primary via-green-400 to-green-300 to-90% rounded-xl text-white" />
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-primary">Portal</span>
+        </h1>
+      </Link>
+      <div className="flex items-center gap-4">
+        <ModeToggle />
+        <Button className="cursor-pointer">Login</Button>
+      </div>
+    </nav>
+  );
+}
